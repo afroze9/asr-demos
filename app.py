24f2b6c4b304834273c29b97aff8e20959529831
afroze9 <afrozeamjad@gmail.com>
2024-11-11 23:36:41 +0500

    Transcribe API using Whisper 3 Local

---
+++ app.py
@@ -0,0 +1,46 @@
+import torch
+import uvicorn
+import numpy as np
+import soundfile as sf
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from fastapi import FastAPI, UploadFile, File
+from io import BytesIO
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+print(device)
+print(torch_dtype)
+
+model_id = "openai/whisper-large-v3-turbo"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+    return_timestamps=True
+)
+
+app = FastAPI()
+
+
+@app.post("/transcribe/")
+async def transcribe(file: UploadFile = File(...)):
+    audio_bytes = await file.read()
+    audio, _ = sf.read(BytesIO(audio_bytes))
+    result = pipe(audio)
+
+    return result
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
