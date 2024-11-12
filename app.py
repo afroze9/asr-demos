306e6c24ceba8f12ad1a25c0301fd657004878ac
afroze9 <afrozeamjad@gmail.com>
2024-11-12 13:46:18 +0500

    Separate api file

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import soundfile as sf
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from fastapi import FastAPI, UploadFile, File
 from io import BytesIO
+import wave
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
@@ -37,6 +38,7 @@ app = FastAPI()
 @app.post("/transcribe/")
 async def transcribe(file: UploadFile = File(...)):
     audio_bytes = await file.read()
+
     audio, _ = sf.read(BytesIO(audio_bytes))
     result = pipe(audio)
 
